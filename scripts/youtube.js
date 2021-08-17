4b208e81e3b5551fb5cf2c63c712fea946490e10
Walker Peredo <walkerperedo@gmail.com>
2021-08-16 22:53:09 -0400

    selecting all trending videos

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -3,7 +3,12 @@ module.exports = async (page, website) => {
   await page.goto(url);
   await page.waitForSelector(selectors.trendsLink);
   await page.click(selectors.trendsLink);
-  await page.evaluate(() => {
-    console.log("hola estoy en el navegador");
-  });
+  // para pasar variable al evaluate se le hace a travez del segundo argumento del evaluate
+  await page.waitForSelector(selectors.trendListTags);
+  await page.evaluate((trendListTags) => {
+    const trendList = document.querySelectorAll(trendListTags);
+    for (const trend of trendList) {
+      console.log(trend.innerText);
+    }
+  }, selectors.trendListTags);
 };
