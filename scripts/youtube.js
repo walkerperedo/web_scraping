8c94247aa7899de7c3a3c90bdb9a16fe53b04b34
Walker Peredo <walkerperedo@gmail.com>
2021-08-16 23:22:28 -0400

    clean and filter the information

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -1,10 +1,14 @@
 module.exports = async (page, website) => {
   const { url, selectors } = website;
   await page.goto(url);
+
   await page.waitForSelector(selectors.trendsLink);
+
   await page.click(selectors.trendsLink);
+
   // para pasar variable al evaluate se le hace a travez del segundo argumento del evaluate
   await page.waitForSelector(selectors.trendListTags);
+
   const trendsText = await page.evaluate((trendListTags) => {
     const trendList = document.querySelectorAll(trendListTags);
     const trendsText = [];
@@ -16,5 +20,9 @@ module.exports = async (page, website) => {
     return trendsText;
   }, selectors.trendListTags);
 
-  console.log(trendsText);
+  const regExp = new RegExp("[A-z]+");
+  for (const text of trendsText) {
+    const textSplitted = text.split("\n").filter((txt) => regExp.test(txt));
+    console.log(textSplitted);
+  }
 };
