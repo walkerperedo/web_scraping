d41cc1cb9ca168e8c22ee4ddeda3e40cc3ffb5e5
Walker Peredo <walkerperedo@gmail.com>
2021-08-18 23:33:41 -0400

    go to marketplace

--- scripts/facebook.js
+++ scripts/facebook.js
@@ -8,4 +8,6 @@ module.exports = async (page, website) => {
   await page.type(selectors.passwordInput, process.env.FACEBOOK_PASSWORD);
   await page.waitForSelector(selectors.signInbutton);
   await page.click(selectors.signInbutton);
+  await page.waitFor(3000);
+  await page.goto(`${website.url}/marketplace`);
 };
