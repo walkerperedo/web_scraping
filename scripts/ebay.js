ec02e657ed1fba99c2f99c6e8c6fcf1baa510eb2
Walker Peredo <walkerperedo@gmail.com>
2021-08-23 23:58:40 -0400

    select al title ebay

--- scripts/ebay.js
+++ scripts/ebay.js
@@ -7,4 +7,17 @@ module.exports = async (page, website) => {
   await page.goto(website.url);
   await page.waitForSelector(selectors.searchBar);
   await page.type(selectors.searchBar, "laptops");
+  await page.waitForSelector(selectors.searchButton);
+  await page.click(selectors.searchButton);
+  await page.waitForSelector(selectors.itemTitle);
+  await page.evaluate((itemTitle) => {
+    const titles = document.querySelectorAll(itemTitle);
+    const parsedTitles = [];
+
+    for (const title of titles) {
+      parsedTitles.push(title.innerText);
+    }
+
+    console.log(parsedTitles);
+  }, selectors.itemTitle);
 };
