f0a6e8c7850011ff2d818b11d29d0def4753066a
Walker Peredo <walkerperedo@gmail.com>
2021-08-23 23:43:46 -0400

    ebay select the bar and search

--- scripts/ebay.js
+++ scripts/ebay.js
@@ -1,4 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 
-module.exports = async (page, website) => {};
+module.exports = async (page, website) => {
+  const { selectors } = website;
+
+  await page.goto(website.url);
+  await page.waitForSelector(selectors.searchBar);
+  await page.type(selectors.searchBar, "laptops");
+};
